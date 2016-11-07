34db4ab35efa0f8491e2ff251cd9f73beec2f1d5
orion <taranov.vv@gmail.com>
2016-11-08 00:06:47 +0300

    typecheck with class decorator

--- tests/test_typing_descriptor.py
+++ tests/test_typing_descriptor.py
@@ -2,6 +2,8 @@ import pytest
 
 from scripts.typing_descriptor import Integer
 
+from scripts.typing_descriptor import Typed, type_assert
+
 @pytest.fixture()
 def point():
 	class Point:
@@ -18,3 +20,31 @@ def test_integer(point):
 	assert point.x == 0
 	point.x = 42
 	assert point.x == 42
+
+	with pytest.raises(TypeError):
+		point.x = 23.1
+
+
+@pytest.fixture
+def stock():
+	@type_assert(name=str, shares=int, price=float)
+	class Stock:
+		def __init__(self, name, shares, price):
+			self.name = name
+			self.shares = shares
+			self.price = price
+
+	return Stock('NAME', shares=42, price=23.4)
+
+def test_class_decorator(stock):
+	assert stock.name == 'NAME'
+	with pytest.raises(TypeError):
+		stock.name = 23
+	stock.name += 'qwe'
+	assert stock.name == 'NAMEqwe'
+
+	assert stock.shares == 42
+	with pytest.raises(TypeError):
+		stock.shares += 'NAME'
+
+	stock.shares = 100500
